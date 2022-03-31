2bb3828f517ae801879f4a8067cccf7a8affd07c
Sunny Israni <sunnyisrani@dyn-209-2-230-226.dyn.columbia.edu>
2022-03-31 19:04:00 -0400

    Remaining workflow stuff

--- app/jobs/generate_new_user_job.rb
+++ app/jobs/generate_new_user_job.rb
@@ -1,10 +1,10 @@
-class GenerateNewUserJob
-  include Sidekiq::Job
-
-  def perform
+class GenerateNewUserJob < BaseJob
+  def execute(args)
+    p '*'*500
     user = User.last
     last_name = "#{user.last_name} #{1}"
     sleep(5)
+    # raise StandardError, "This job has failed!"
     User.create(first_name: user.first_name, last_name: last_name, email: user.email)
   end
 end
